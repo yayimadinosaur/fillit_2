97c0257a19b6e81ba6157a5e8a40bbd285c87a4e
warren fung <wfung@e1z2r6p5.42.us.org>
2017-02-03 19:15:35 -0800

    added \n and . chars for build_base

--- ft_build_map.c
+++ ft_build_map.c
@@ -6,7 +6,7 @@
 /*   By: wfung <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/03 18:22:15 by wfung             #+#    #+#             */
-/*   Updated: 2017/02/03 18:39:38 by wfung            ###   ########.fr       */
+/*   Updated: 2017/02/03 19:14:46 by wfung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,27 @@ t_list	*ft_fill_map(t_list *base, char *str, int n)//str = perfect str n = # of 
 
 t_list	*ft_build_base(char *str, int n)	//str = perfect str n = # of shapes/size?
 {
-	int	i;	//counter for base # for square (formula = i^2 + i + 1)
-			//							i^2 = space for chars, i = \n, 1 = null
+	int	i;	//counter for base # for square
+	int	j;	//counter for each t_list + formula
+
 	t_list	*head;		//head for list
 	t_list	*buff;		//make list
 	
-	i = 2;	//start off at a 2x2 square; total = 2^2 + 2 + 1 => 7
-	if (!(buff = (*t_list)malloc(sizeof(t_list) * ((i * i) + i + 1))))
+	i = 0;
+	j = (n * n) + n + 1;	//i^2 = space for chars, i = \n, 1 = null
+	if (!(buff = (*t_list)malloc(sizeof(t_list) * j)))
 		return (0);		//>>>test if return 0 is correct if malloc fails<<<
+	buff[j] = '\0';
+	j = 0;		//reset j to use as counter??<<<<<
+	while (buff[i] != '\0')
+	{
+		if (i % (n + 1) == 0)
+			buff->data = '\n';
+		else
+			buff->data = '.';
+	}
 	return (buff);
 }
+
+	//start off at a 2x2 square; total = 2^2 + 2 + 1 => 7
+
